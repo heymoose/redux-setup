2b1d483822aa7b56d5fbbc5e2e7d2ff671a65a23
Marc Novak <marcus.novak@asynchrony.com>
2019-05-16 09:12:14 -0600

    Add header to stories grid

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import StoriesHeader from './StoriesHeader';
 import Story from './Story';
 import './Stories.css';
 
+const COLUMNS = {
+	title: {
+		label: 'Title',
+		width: '40%'
+	},
+	author: {
+		label: 'Author',
+		width: '30%'
+	},
+	comments: {
+		label: 'Comments',
+		width: '10%'
+	},
+	points: {
+		label: 'Points',
+		width: '10%'
+	},
+	archive: {
+		width: '10%'
+	}
+};
+
 const Stories = ({ stories }) => (
-	<div className="stories">{(stories || []).map((story) => <Story key={story.objectID} story={story} />)}</div>
+	<div className="stories">
+		<StoriesHeader columns={COLUMNS} />
+		{(stories || []).map((story) => <Story key={story.objectID} story={story} columns={COLUMNS} />)}
+	</div>
 );
 
 export default Stories;
