c81145a890d1cce91ca926462fe07939b2c2141f
Marc Novak <marcus.novak@asynchrony.com>
2019-05-16 09:36:14 -0600

    Add callback for story click

--- src/index.js
+++ src/index.js
@@ -23,9 +23,6 @@ const stories = [
 	}
 ];
 
-ReactDOM.render(<App stories={stories} />, document.getElementById('root'));
+ReactDOM.render(<App stories={stories} onArchive={() => {}} />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
